3e56436eb639c2cf146f3e303ae67071adf46ca2
Sam Schmit-Van Werweke <sam@schm.it>
2020-04-13 10:33:04 +0200

    Improve image size by using multi-stage builds and copying required files, add support for JRE builds

--- docker.sh
+++ docker.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 VERSION=$1
-TAGS=$2
-VARIANT=$3
-VERSION_ONLY_TAG=$4
-VERSIONLESS_TAG=$5
+TAG=$2
+JAVA_BASE=$3
+TAGS=$4
+VARIANT=$5
+VERSION_ONLY_TAG=$6
 
 function tag_and_push() {
   docker tag samschmit/tomcat-hardened samschmit/tomcat-hardened:"$1"
@@ -13,18 +14,16 @@ function tag_and_push() {
 
 # Create list of tags to process
 read -ra TAGLIST <<< "${TAGS//,/ }" # replace "," with " " and put into array
-if [ "$VERSION_ONLY_TAG" ]; then
-  TAGLIST+=("") # append empty tag for version only tags
-fi
 
 MAJOR_MINOR=$(echo "$VERSION" | cut -d "." -f 1-2)
 MAJOR=$(echo "$VERSION" | cut -d "." -f 1)
 
 # Prepare docker file
-sed "s/#BASE#/$VERSION-${TAGLIST[0]}/g" "Dockerfile.$VARIANT" > Dockerfile
+sed "s/#TOMCAT_BASE#/$VERSION-$TAG/g" "Dockerfile.$VARIANT" > Dockerfile
+sed -i "s/#JAVA_BASE#/$JAVA_BASE/g" Dockerfile
 
 # Build docker image
-docker build --build-arg VERSION=$MAJOR_MINOR -t samschmit/tomcat-hardened .
+docker build --build-arg VERSION="$MAJOR_MINOR" -t samschmit/tomcat-hardened .
 
 # Login
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
@@ -35,7 +34,14 @@ for tag in "${TAGLIST[@]}"; do
   tag_and_push "$MAJOR_MINOR-$tag"
   tag_and_push "$MAJOR-$tag"
 
-  if [[ $VERSIONLESS_TAG && -n $tag ]]; then
+  # versionless tag for v8.5
+  if [[ $MAJOR_MINOR == "8.5" && -n $tag ]]; then
     tag_and_push "$tag"
   fi
 done
+
+if [[ "$VERSION_ONLY_TAG" == "true" ]]; then
+  tag_and_push "$VERSION"
+  tag_and_push "$MAJOR_MINOR"
+  tag_and_push "$MAJOR"
+fi
